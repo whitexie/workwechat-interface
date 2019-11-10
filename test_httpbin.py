1436a050471b80309264108fb4a273303c97fc12
xiebo <shieber@163.com>
2019-11-10 22:32:40 +0800

    feat: init with poetry

---
+++ test_httpbin.py
@@ -0,0 +1,10 @@
+import requests
+import unittest
+
+
+class TestHttpBin(unittest.TestCase):
+
+    def test_send_get(self):
+        url = "http://httpbin.org/get"
+        resp = requests.get(url)
+        assert resp.status_code == 200
