58f332daf07016bbc1cd36fb55a3c874fbbabe37
shieber <shieber@163.com>
2019-11-12 11:14:19 +0800

    feat: 实现运行yaml脚本并完成校验

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -10,5 +10,10 @@ class TestRunner(unittest.TestCase):
     def test_run_api(self):
         api_path = os.path.join(os.path.dirname(__file__), 'api', 'httpbin_get.yml')
         api_info = load_yaml(api_path)
-        resp = run_api(api_info)
-        self.assertEqual(resp.status_code, 200)
+        success = run_api(api_info)
+        self.assertTrue(success)
+
+        api_path = os.path.join(os.path.dirname(__file__), 'api', 'httpbin_post.yml')
+        api_info = load_yaml(api_path)
+        success = run_api(api_info)
+        self.assertTrue(success)
